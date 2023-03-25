54783f812aacdeff2e10936dbe10d2e6308e1bc3
Bruno William <brunowilliamcalixto@gmail.com>
2023-03-25 15:21:13 -0300

    Updated Archives

---
+++ Desafio_ExercicioProposto_EnumComposicao/Desafio_ExercicioProposto_EnumComposicao/Entities/Order.cs
@@ -0,0 +1,69 @@
+﻿using Desafio_ExercicioProposto_EnumComposicao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_ExercicioProposto_EnumComposicao.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+
+        public double Total()
+        {
+            double totalValue = 0.0;
+
+            foreach (OrderItem item in Items)
+            {
+                totalValue += item.SubTotal();
+            }
+            return totalValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment);
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client.Name + ", (" + Client.birthDate + ")" + " - " + Client.Email);
+            sb.AppendLine();
+            sb.AppendLine("Order items:");
+            sb.AppendLine();
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.Product.Name + ", Quantity: " + item.Quantity
+                    +", SubTotal: $" + item.SubTotal());
+           
+            }
+            sb.AppendLine("----------------------------------------------");
+            sb.AppendLine("Total Price: " + Total());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Desafio_ExercicioProposto_EnumComposicao/Desafio_ExercicioProposto_EnumComposicao/Program.cs
@@ -0,0 +1,61 @@
+﻿using Desafio_ExercicioProposto_EnumComposicao.Entities;
+using Desafio_ExercicioProposto_EnumComposicao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_ExercicioProposto_EnumComposicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Enter birth date(DD/MM/YYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.Write("Enter order data: ");
+            string orderStatus = Console.ReadLine();
+            OrderStatus status = (OrderStatus)Enum.Parse(typeof(OrderStatus), orderStatus);
+
+            
+            DateTime dateNow = DateTime.Now;
+            Order order = new Order(dateNow, status, client);
+
+            Console.Write("How many items to this order? ");
+            int numberItems = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberItems; i++)
+            {
+                Console.WriteLine("Enter #" + i + " item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+
+                OrderItem item = new OrderItem(quantity, new Product(productName, productPrice));
+                order.AddItem(item);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("ORDER SUMARY: ");
+            Console.WriteLine(order);
+        }
+    }
+}
